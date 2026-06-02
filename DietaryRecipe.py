9bed9b8ef726d2c373b9e1d1a6c5abe0bbbe7a7d
nikibobus <nksuloev@edu.hse.ru>
2026-06-02 17:26:00 +0300

    Добавил importы и исправил ошибку в названиях

--- DietaryRecipe.py
+++ DietaryRecipe.py
@@ -1,9 +1,11 @@
+from Recipe import Recipe
+from Ingredient import Ingredient
 class DietaryRecipe(Recipe):
     def __init__(self, title, diet_type, ingredients=None):
         super().__init__(title, ingredients)
-        self.dietary_info = diet_type
+        self.diet_type = diet_type
     def scale(self, ratio: float):
         scaled = super().scale(ratio)
         return DietaryRecipe(scaled.title, self.diet_type, scaled.ingredients)
     def __str__(self):
-        return '[' + str(self.dietary_info) + ']' + super().__str__() 
+        return '[' + str(self.diet_type) + '] ' + super().__str__() 
