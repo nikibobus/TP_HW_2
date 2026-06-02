6c4367ec0a63b3bbc194b2308e910b5db11e6f6d
nikibobus <nksuloev@edu.hse.ru>
2026-06-02 10:42:19 +0300

    Реализовал класс Recipe с нужными функциями

---
+++ Recipe.py
@@ -0,0 +1,26 @@
+class Recipe:
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        self.ingredients = ingredients
+    def add_ingredient(self, ingredient: Ingredient):
+        self.ingredients.append(ingredient)
+    @staticmethod
+    def is_valid_ratio(ratio):
+        try:
+            return float(ratio) > 0
+        except (TypeError, ValueError):
+            return False
+    def scale(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("ratio должен быть положительным числом")
+        new_ingredients = []
+        for x in self.ingredients:
+            new_ingredients.append(Ingredient(x.name, x.quantity * ratio, x.unit))
+        return Recipe(self.title, new_ingredients)
+    def __len__(self):
+        return len(self.ingredients)
+    def __str__(self):
+        result = ''
+        for x in self.ingredients:
+            result += str(x) + '\n'
+        return str(self.title) + '\n' + result
